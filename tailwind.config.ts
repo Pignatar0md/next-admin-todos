3774764a9bb8fed62032d540946378b849abf1da
Marcelo Pignataro <pignataromd@gmail.com>
2026-01-06 10:05:00 +0000

    Prisma migrations and tailwind config file

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./app/pages/**/*.{js,ts,jsx,tsx}",
+    "./app/components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./app/todos/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      bacgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      }
+    },
+  },
+  plugins: [],
+}
